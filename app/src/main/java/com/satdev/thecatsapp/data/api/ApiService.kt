30ef8b375cfab82944f6d3b35e557b2517ba28ca
salbornozt <salbornozt@unbosque.edu.co>
2023-04-13 16:25:27 -0500

    api key

--- app/src/main/java/com/satdev/thecatsapp/data/api/ApiService.kt
+++ app/src/main/java/com/satdev/thecatsapp/data/api/ApiService.kt
@@ -3,8 +3,10 @@ package com.satdev.thecatsapp.data.api
 import com.satdev.thecatsapp.data.model.Breed
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface ApiService {
+    @Headers("API-KEY: x-api-key: bda53789-d59e-46cd-9bc4-2936630fde39")
     @GET("breeds")
     suspend fun getAllBreeds() : Response<List<Breed>>
 }
